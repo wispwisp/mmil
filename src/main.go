002a827848c8a311ded78e05e35440dfddffcd6a
wisp <forworkandtravel@yandex.ru>
2022-08-31 07:37:56 +0000

    use args

--- src/main.go
+++ src/main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/wispwisp/mmil/readers"
 	"github.com/wispwisp/mmil/scales"
 )
 
+type Args struct {
+	ReadFromFile  *bool
+	InputFileName *string
+}
+
+func registerArgs() (args Args) {
+	args.ReadFromFile = flag.Bool("fromfile", true, "read input from file")
+	args.InputFileName = flag.String("filename", "input.txt", "input file name")
+	flag.Parse()
+	return
+}
+
 func main() {
+	args := registerArgs()
 	s := scales.Scales{}
 
 	// TODO: arg to specify kind of input
-	if false {
+	if !*args.ReadFromFile {
 		err := readers.ReadFromInput(&s)
 		if err != nil {
 			fmt.Println("Error reading answers from stdin:", err)
 			return
 		}
-	} else if true {
-		fileName := "./input.txt"
-		err := readers.ReadFromFile(fileName, &s)
+	} else {
+		err := readers.ReadFromFile(*args.InputFileName, &s)
 		if err != nil {
-			fmt.Println("Error reading answers from file", fileName, ", error:", err)
+			fmt.Println("Error reading answers from file", *args.InputFileName, ", error:", err)
 			return
 		}
 	}
