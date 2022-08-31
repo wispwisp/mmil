943d301836200397f092f8ac928330fec3762c09
wisp <forworkandtravel@yandex.ru>
2022-08-31 08:03:05 +0000

    print welsh index

--- src/scales/result.go
+++ src/scales/result.go
@@ -15,6 +15,7 @@ type Result struct {
 
 func (r *Result) write(out io.Writer) {
 	// TODO: gender in result
+	fmt.Fprintln(out, "Welsh index:", r.WelshIndex)
 	scaleNames := []string{"L", "F", "K", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 	fmt.Fprintf(out, "Raw data | \t T-scales:\n")
 	for i := 0; i < SCALESLIM; i++ {
